a69eec4cce87d525362c3143b4cab1296673637f
rabiiii3 <108283915+rabiiii3@users.noreply.github.com>
2022-07-01 21:31:14 +0200

    Update streamlit_app.py
    afficher la réponse de l'api

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # API REQUEST.
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
